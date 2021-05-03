f7eb016cbf544732616ba7c754218b0ccc4bd37b
groupAcsc350 <82403644+groupAcsc350@users.noreply.github.com>
2021-05-03 17:59:25 -0400

    Update and rename first page html - louise gui to login.php

--- login.php
+++ login.php
@@ -14,15 +14,24 @@
       <h1> <img src="https://i.ibb.co/6WndSY1/Pin-Clipart-com-machines-clipart-739756.png" height="60px" width="50px"><br></br> Laundry Room Sign up </h1>
       <p> Please enter your apartment number and password assigned</p>
       <h2>Sign in</h2>
-      <form action="action_page.php" method="post">
+      <form  name="login" action="authenticate.php" onsubmit="return validateLogin();" method="post">
      <label for="username"><br></br></label>
-    <input class="username" type="ext" placeholder="Apartment number" name="username" required>
+    <input class="username" type="ext" placeholder="Apartment number" name="username" id="username" required="required">
+
     <label for="password"><br></br></label>
-    <input class="password" type="password" placeholder="Password" name="password" required>
-    <input  class="button"   type="submit" value="Login">
+    <input class="password" type="password" placeholder="Password" name="password" id="password" required="required">
+
+    <input  class="button"   type="submit" value="Login"></a>
+	<?php
+                    if(isset($_SESSION["error"])){
+                        $error = $_SESSION["error"];
+                        echo "<span>$error</span>";
+                    }
+                ?>  
 	
-  </div>
-  </form>
+  
+  </div></div></form>
+    </div>
   </body>
 </html>
 
