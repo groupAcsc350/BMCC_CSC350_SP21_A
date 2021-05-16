c33be86ee2d54be94e6df160f7988d4aa347c20e
groupAcsc350 <82403644+groupAcsc350@users.noreply.github.com>
2021-05-16 19:49:21 -0400

    Create reserve.php

---
+++ reserve.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+
+?>
+<html>
+<head>
+<title>RESERVATION PAGE</title>
+</head>
+<body>
+
+<?php
+if ($_SESSION["username"])
+{
+?>
+
+
+<?php
+
+    $con = mysqli_connect('localhost', 'root', 'root', 'laundry') or die('Unable To connect');
+    $test = $_SESSION["username"];
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $slot = isset($_POST["slot"]) ? $_POST["slot"] : '';
+
+        $result = mysqli_query($con, "SELECT username FROM reservations WHERE username = '" . $test . "'");
+        $result2 = mysqli_query($con, "SELECT slot FROM reservations WHERE username =  '" . $test . "'");
+
+        $row = mysqli_fetch_assoc($result2);
+        $selected_slot = "You already have a reservation: " . $row['slot'] . " Would you like to cancel this reservation? Click 'OK' to cancel or 'Cancel' to keep your reservation.";
+
+        if (mysqli_num_rows($result) == 0)
+        {
+            $sql = "INSERT INTO reservations (username, slot) VALUES ('" . $test . "', '" . $slot . "')";
+
+        }
+        else
+        {
+            echo '<script type="text/javascript">confirm( "' . $selected_slot . '");window.location=\'cancelreservation.php\';</script>';
+
+        }
+
+        if (mysqli_query($con, $sql))
+        {
+            echo "Records inserted successfully." . $slot . $_SESSION["username"];
+        }
+        else
+        {
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+        } ?>
+<?php
+    }
+    else echo "<h1>Please login first .</h1>";
+}
+
+
+?>
+
+
+	
+</body>
+</html>
