08b2725a668c50abbd8a246abf286a57ea925959
groupAcsc350 <82403644+groupAcsc350@users.noreply.github.com>
2021-05-16 19:46:01 -0400

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,43 +1,35 @@
-
 <?php
-$user = isset($_POST["username"]) ? $_POST["username"] : '';
-$pass = isset($_POST["password"]) ? $_POST["password"] : '';
+session_start();
 
- 
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = 'root';
-$DATABASE_NAME = 'laundry';
-
-
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-
-
-if(! $con)
+if (count($_POST) > 0)
 {
-     error_log("Failed to connect to MySQL: " . mysqli_error($con));
-    die('Internal server error');
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_username = 'root';
+    $DATABASE_PASS = 'root';
+    $DATABASE_NAME = 'laundry';
+
+    $con = mysqli_connect($DATABASE_HOST, $DATABASE_username, $DATABASE_PASS, $DATABASE_NAME) or die('Unable To connect');
+
+    $username = isset($_POST["username"]) ? $_POST["username"] : '';
+    $password = isset($_POST["password"]) ? $_POST["password"] : '';
+
+    $sql = "select username, password from users where username = '" . $username . "'";
+
+    $result = mysqli_query($con, $sql) or die(mysqli_error($con));
+
+    $row = mysqli_fetch_array($result);
+
+    if ($row["username"] == $username && $row["password"] == $password)
+    {
+        $_SESSION["username"] = $row['username'];
+        $_SESSION["password"] = $row['password'];
+        header("Location:reservation.php");
+    }
+    else
+    {
+        echo '<script type="text/javascript">alert("Wrong username or password. If you are having problems with your login credentials please contact us (000) 000-0000 or try again.");window.location=\'login.php\';</script>';
+
+    }
 }
- 
-$db_select = mysqli_select_db($con, "laundry");
-if (!$con) {
-    error_log("Database selection failed: " . mysqli_error($con));
-    die('Internal server error');
-}
 
-$sql = "select username, password from users where username = '".$user."'";
-
-$result = mysqli_query($con,$sql) or die( mysqli_error($con));
-
-
-$row = mysqli_fetch_array($result);
-
-if($row["username"]==$user && $row["password"]==$pass)
-    include "reservation.php";
-else
-	echo '<script type="text/javascript">alert("Wrong username or password. If you are having problems with your login credentials please contact us (000) 000-0000 or try again.");window.location=\'login.php\';</script>';
 ?>
-
-
-
-
