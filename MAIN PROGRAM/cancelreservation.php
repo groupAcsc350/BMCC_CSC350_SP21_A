0ee6a2ad8afe574cec9d7052382052f3283c50ad
groupAcsc350 <82403644+groupAcsc350@users.noreply.github.com>
2021-05-17 18:01:39 -0400

    Update and rename cancelreservation.php to MAIN PROGRAM/cancelreservation.php

---
+++ MAIN PROGRAM/cancelreservation.php
@@ -0,0 +1,27 @@
+
+<?php
+session_start();
+
+?>
+
+<?php
+
+if($_SESSION["username"]) {
+?>
+<?php
+$con = mysqli_connect('localhost', 'root', 'root', 'laundry') or die('Unable To connect');
+$username = $_SESSION["username"] ;
+$slot = isset($_POST["slot"]) ? $_POST["slot"] : '';
+
+$result = mysqli_query($con,"SELECT username, slot FROM reservations WHERE username = '".$username."' and slot = '".$slot."'");
+
+	if (!empty($result) ){
+	$delete_reservation  = "DELETE FROM reservations WHERE username = '".$username."' and slot = '".$slot."'";
+	mysqli_query($con,$delete_reservation);
+	echo '<script type="text/javascript">alert("Reservation cancelled. Feel free to make a new reservation for a different time.");window.location=\'rs.php\';</script>';
+
+}
+}
+
+?>
+
