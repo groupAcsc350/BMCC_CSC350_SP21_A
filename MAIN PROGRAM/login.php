4529d0b36d81c244e4eb5fc148698f03af092589
groupAcsc350 <82403644+groupAcsc350@users.noreply.github.com>
2021-05-24 15:44:30 -0400

    Update login.php

--- MAIN PROGRAM/login.php
+++ MAIN PROGRAM/login.php
@@ -13,15 +13,19 @@
     <div class="box">
       <h1> <img src="https://i.ibb.co/6WndSY1/Pin-Clipart-com-machines-clipart-739756.png" height="60px" width="50px"><br></br> Laundry Room Sign up </h1>
       <p> Please enter your apartment number and password assigned</p>
-      <h2>Sign in</h2>
+
       <form  name="login" action="authenticate.php" onsubmit="return validateLogin();" method="post">
      <label for="username"><br></br></label>
-    <input class="username" type="text" placeholder="Apartment number" name="username" id="username" required="required">
+    <input class="username"  placeholder="Apartment number" name="username" id="username" required="required">
 
     <label for="password"><br></br></label>
     <input class="password" type="password" placeholder="Password" name="password" id="password" required="required">
 
-    <input  class="button"   type="submit" value="Login"></a>
+    <input  class="button"   type="submit" value="Login"></a><br></br>
+
+	
+	 <p style="background:white; font-size: 15px; color: black;">Forgot your username or password? Click <a style="text-decoration: none; font-size: 15px; color: white; background: black; border-radius: 10px;" target="_blank" href="credentials.html">here.</a> <br></br></p>
+   
 	
   
   </div></div></form>
