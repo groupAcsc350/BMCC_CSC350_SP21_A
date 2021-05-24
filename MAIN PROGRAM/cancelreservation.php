ea3ed93671d2ae91c2f1312aabc62b8b39e7783c
groupAcsc350 <82403644+groupAcsc350@users.noreply.github.com>
2021-05-23 21:52:35 -0400

    Update cancelreservation.php

--- MAIN PROGRAM/cancelreservation.php
+++ MAIN PROGRAM/cancelreservation.php
@@ -13,15 +13,15 @@ $con = mysqli_connect('localhost', 'root', 'root', 'laundry') or die('Unable To 
 $username = $_SESSION["username"] ;
 $slot = isset($_POST["slot"]) ? $_POST["slot"] : '';
 
-$result = mysqli_query($con,"SELECT username, slot FROM reservations WHERE username = '".$username."' and slot = '".$slot."'");
+$result = mysqli_query($con,"SELECT username, slot FROM reservations WHERE username = '".$username."'");
 
 	if (!empty($result) ){
-	$delete_reservation  = "DELETE FROM reservations WHERE username = '".$username."' and slot = '".$slot."'";
+	$delete_reservation  = "DELETE FROM reservations WHERE username = '".$username."'" ;
+	
 	mysqli_query($con,$delete_reservation);
-	echo '<script type="text/javascript">alert("Reservation cancelled. Feel free to make a new reservation for a different time.");window.location=\'rs.php\';</script>';
+	echo '<script type="text/javascript">alert("Reservation cancelled. Feel free to make a new reservation for a different time.");window.location=\'reservation.php\';</script>';
 
-}
-}
+}}
 
 ?>
 
